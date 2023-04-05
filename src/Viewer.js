459e21ac00c02da2c1fbf9614b4c03ec1df8062d
k-e-n-5 <kengo.afternoon.tea@gmail.com>
2023-04-05 15:59:38 +0000

    メッセージ表示画面修正（URLパラメタ化）

--- src/Viewer.js
+++ src/Viewer.js
@@ -1,51 +1,30 @@
 import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Web3 from 'web3';
 import abi from "./contracts/GiftNFT.json";
 
 function Viewer() {
-    const [address, setAddress] = useState();
-    const [tokenId, setTokenId] = useState();
-    const [collection, setCollection] = useState("-");
-    const [contract, setContract] = useState(null);
-    const [web3, setWeb3] = useState(null);
+    const [ req, setReq ] = useState(new URLSearchParams(useLocation().search));
+    const [ message, setMessage] = useState();
+    const [ id, setId ] = useState(req.get('id'));
+    const [ web3, setWeb3 ] = useState(new Web3());
 
-    const getCollection = async () => {
-        const contractAddress = "0x4AAbbb31f44941eFAd14f019Efe876916b57a293";
-        const contract = new web3.eth.Contract(abi, contractAddress);
-        const message = await contract.methods.tokenMessage(tokenId).call();
-	setCollection(message);
-	const tran = contract.getTransaction();
-	console.log("FINISH");
-    }
+    web3.setProvider(new web3.providers.HttpProvider('https://rpc-mumbai.maticvigil.com/'));
+    const contractAddress = "0x4AAbbb31f44941eFAd14f019Efe876916b57a293";
+    const contract = new web3.eth.Contract(abi, contractAddress);
+    const nft = contract.methods.tokenMessage(id).call();
 
     useEffect(() => {
-        if (typeof window.ethereum !== 'undefined') {
-	    try {
-		const web3 = new Web3(window.ethereum);
-		setWeb3(web3);
-	    } catch (error) {
-		console.error(error);
-	    }
-        } else {
-	    console.error("Please install MetaMask!");
-	}
-    }, []);
-return(
-    <div>
-        <h1>NFT確認</h1>
-        <div>
-            <h2>Account</h2>
-	</div>
-        <div>
-            NFTAddress:<input type="text" name="address" value={address} onChange={(e) => setAddress(e.target.value)} />
-	    <br/>
-            TokenId:<input type="text" name="tokenId" value={tokenId} onChange={(e) => setTokenId(e.target.value)} />
-	    <br/>
-            <button id="getColection" onClick={getCollection} >GetCollection</button>
-	    <br/>
-            Collection:{collection}
-        </div>
-    </div>
+        (async() => {
+            web3.setProvider(new web3.providers.HttpProvider('https://rpc-mumbai.maticvigil.com/'));
+            const contractAddress = "0x4AAbbb31f44941eFAd14f019Efe876916b57a293";
+            const contract = new web3.eth.Contract(abi, contractAddress);
+            const nft = await contract.methods.tokenMessage(id).call();
+            setMessage(nft);
+        })()
+    });
+    return (
+        <>{message}</>
     );
 };
 
